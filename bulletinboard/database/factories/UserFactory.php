8d8d54862dfe05fca6603c099e3d5e124c63207a
yinyinayescm <60867210+yinyinayescm@users.noreply.github.com>
2020-05-26 10:29:13 +0630

    change

--- bulletinboard/database/factories/UserFactory.php
+++ bulletinboard/database/factories/UserFactory.php
@@ -2,7 +2,8 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
-use App\User;
+use App\Models\User;
+use App\Models\Post;
 use Faker\Generator as Faker;
 use Illuminate\Support\Str;
 
@@ -19,10 +20,29 @@ use Illuminate\Support\Str;
 
 $factory->define(User::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-        'remember_token' => Str::random(10),
+        'id' =>'1',
+        'name' => 'admin',
+        'email' => 'admin@gmail.com',
+        'password' => '$2y$12$QbDu1X.nhAzO1M2ynNtW3uyjkuPSi09VijhQUkGGgbcvpm87U/1ju', // password
+        'profile' => Str::random(10).'.jpg',
+        'type' => 1,
+        'phone' => $faker->phoneNumber,
+        'address' => $faker->address,
+        'dob' => $faker->date,
+        'create_user_id' => 1,
+        'updated_user_id' => 1,
+        'created_at'=> now(),
+        'updated_at'=> now(),
+    ];
+});
+$factory->define(Post::class, function (Faker $faker) {
+    return [
+        'title' => $faker->catchPhrase,
+        'description' => $faker->text($maxNbChars = 100),
+        'status' => 1,
+        'create_user_id' => 1,
+        'updated_user_id' => 1,
+        'created_at' => now(),
+        'updated_at' => now(),
     ];
 });
--- bulletinboard/database/seeds/PostsTableSeeder.php
+++ bulletinboard/database/seeds/PostsTableSeeder.php
@@ -11,6 +11,6 @@ class PostsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        factory(App\Models\Post::class, 10)->create();
     }
 }
